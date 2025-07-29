5765cd0bd383bf25ab13d618f5a11c96430272ce
antso <hei.antso.2007@gmail.com>
2025-07-29 13:30:59 +0300

    ajout supplémentaire du front end

--- pokedex/src/containers/pokedex.jsx
+++ pokedex/src/containers/pokedex.jsx
@@ -35,7 +35,8 @@ export default function Pokedex() {
         })
         .catch((error) => {
             console.error("le site ne fonctionne pas");
-        });
+        })
+    
     }
 
     useEffect(() => {
@@ -43,37 +44,52 @@ export default function Pokedex() {
     }, []);
 
 
+
    return (
         <div>
-            <div>
-                <input type="text" />
-                <button className="bg-blue-500 h-10 text-white p-2 rounded" onClick={toSearch}>Search</button>
-                <button onClick={getAll}>reset</button>
+            <div className="flex flex-row justify-evenly items-center p-5 bg-blue-500">
+                <div>
+                    <img src="logoPokedex.png" alt="" />
+                    logo</div>
+                <div className="flex flex-row">
+                    <div className="bg-white p-2 w-90 rounded-full shadow-lg">
+                        <input type="text" className="h-5 ml-5"/>
+                    <button className="ml-5 bg-gray-100 w-20 rounded-sm" onClick={toSearch}>Search</button>
+                    </div>
+                   <button onClick={getAll} className="ml-5 w-20 h-8 mt-1 rounded full bg-blue-200">reset</button>
+                </div>
+ 
             </div>
-       <div className="grid grid-cols-5 gap-4">
-        {pokemon.map((pokemonList, id) => (
-            <div>
-           <div className="">
-               <div key={id} className="">
-                    <div className="bg-white p-4 rounded-lg shadow-lg">
-                        <img src={pokemonList?.sprites?.front_default} alt="" />
-                        <h2>{pokemonList?.name}</h2>
-                        <p>Types: {pokemonList?.types.map(type => type.type.name).join(', ')}</p>
-                        <button onClick={() => toggleModal(id)} className="bg-blue-500 h-10 text-white p-2 rounded">more info</button> 
+            <div className="grid grid-cols-5 gap-4 mt-20">
+                {pokemon.map((pokemonList, id) => (
+                    <div key={id} className="">
+                        <div className="">
+                            <div className="">
+                                <div className="bg-white p-4 rounded-lg shadow-lg">
+                                    <div className="flex flex-row items-center">
+                                        <div>
+                                            <img src={pokemonList?.sprites?.front_default} alt="" className="items-center justify-center"/>
+                                        </div>
+                                        <div>
+                                    <h2 className="text-center">{pokemonList?.name}</h2>
+                                    <p className="text-center">Types: {pokemonList?.types.map(type => type.type.name).join(', ')}</p>
+                                        </div>
+                                    </div>
+                                    <button onClick={() => toggleModal(id)} className="bg-blue-500 ml-10 w-30 h-8 text-sm text-white p-2 rounded">more info</button> 
                     </div>
                 </div> 
             </div>
             </div>   
        ))}
 
-       <div className="absolute">{pokemon
-       .map((p, id) => (
-           <div key={id} className="">
-                {modal && card === id && (
-                    <div className="fixed bg-white ml-18 mt-6 w-300 h-130 align-middle rounded-lg shadow-lg p-4 ">
-                            <h2 className="text-center bold">{p?.name}</h2> 
-                            <img src={p?.sprites?.front_default} alt="" className="ml-120 h-50"/> 
-                        <div className="flex flex-row items-center">                        
+            <div className="absolute ">{pokemon
+                .map((p, id) => (
+                    <div key={id} className="">
+                        {modal && card === id && (
+                            <div className="fixed bg-white ml-18 w-300 h-110 mb-40 align-middle rounded-lg shadow-lg p-4 ">
+                                <h2 className="text-center bold">{p?.name}</h2>
+                                <img src={p?.sprites?.front_default} alt="" className="ml-120 h-50 " />
+                                <div className="flex flex-row items-center">
                             <div>
                                 <p>Height: {p?.height}</p>
                                 <p>Weight: {p?.weight}</p>
